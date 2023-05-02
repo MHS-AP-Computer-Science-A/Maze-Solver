d416f94fa6dd22006eeb2a140995a3b1d5098ae5
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-05-02 16:13:10 -0400

    Add files via upload

---
+++ src/MazeSolver.java
@@ -0,0 +1,31 @@
+
+public class MazeSolver {
+	
+	public static void main(String[] args) {
+		int[][] maze = 
+			   {{1,1,0,1,1,1,3},
+				{2,0,0,0,1,1,0},
+				{1,1,1,0,1,1,0},
+				{3,0,1,0,0,0,0},
+				{1,0,0,0,1,1,1},
+				{3,1,1,0,1,0,1},
+				{0,0,0,0,0,0,1}};
+		
+		//TODO: find the start of the maze
+		
+		//TODO: call the solve method
+		
+	}
+	
+	public static void solve(int[][] maze, int row, int col) {
+		if(maze[row][col] == 3) {
+			//Base case, found cupcake
+			System.out.println("Cupcake found at (" + row + ", " + col + ")");
+		}
+		else {
+			//TODO: Recursively check other directions
+			
+		}
+		
+	}
+}
