0a6e182fc68e34719225d64b89c3c5a888e62716
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2026-03-16 07:56:55 -0400

    Refactor MazeSolver to handle multiple mazes
    Updated MazeSolver to include visited array and multiple mazes.

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,41 +1,76 @@
 
 public class MazeSolver {
-	
-	public static void main(String[] args) {
-		int[][] maze = 
-			   {{1,1,0,1,1,1,3},
-				{2,0,0,0,1,1,0},
-				{1,1,1,0,1,1,0},
-				{3,0,1,0,0,0,0},
-				{1,0,0,0,1,1,1},
-				{3,1,1,0,1,0,1},
-				{0,0,0,0,0,0,1}};
-		
-		// Create a boolean array named 'visited' that has the same
-		// number of rows as cols as 'maze'. The values default to 'false'.
-	
-		
-		// Find the start of the maze
-		
-		
-		// Call the solve method
-		
+
+	/* Recursively solves a maze by exploring all possible
+	 * paths
+	*/
+	public static void solve(int[][] maze, boolean[][] visited, int row, int col) { 
+		// Mark location as visited
+
+		// If location is 3, print result (base case)
+
+		// Recursively explore up, right, left, down aacording to the algorithm
+		// rules
+
 	}
-	
-	public static void solve(int[][] maze, int row, int col) { // Add visited array as method paramter.
-		// Set visited[row][col] to true
-		
-		if(maze[row][col] == 3) {
-			//Base case, found cupcake
-			System.out.println("Cupcake found at (" + row + ", " + col + ")");
-		}
-		else {
-			int numRows = maze.length;
-			int numCols = maze[0].length;
-			
-			// Recursively check up, down, left, right
-			
-		}
-		
+
+	public static void main(String[] args) {
+		// MAZE 1 
+		int[][] maze1 = 
+		      { { 1, 1, 0, 1, 1, 1, 3 },
+				{ 2, 0, 0, 0, 1, 1, 0 },
+				{ 1, 1, 1, 0, 1, 1, 0 },
+				{ 3, 0, 1, 0, 0, 0, 0 },
+				{ 1, 0, 0, 0, 1, 1, 1 },
+				{ 3, 1, 1, 0, 1, 0, 1 },
+				{ 0, 0, 0, 0, 0, 0, 1 } };
+
+		boolean[][] visited1 = new boolean[maze1.length][maze1[0].length];
+
+		//MAZE 2
+		int[][] maze2 = 
+		      { { 1, 0, 0, 0, 3, 1, 0, 0, 0, 3 },
+				{ 1, 0, 1, 3, 1, 1, 0, 1, 1, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+				{ 1, 0, 1, 1, 1, 0, 0, 0, 0, 0 },
+				{ 1, 0, 1, 1, 1, 0, 1, 1, 1, 3 },
+				{ 1, 0, 1, 1, 1, 0, 0, 0, 1, 1 },
+				{ 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 },
+				{ 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 },
+				{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 2 } };
+
+		boolean[][] visited2 = new boolean[maze2.length][maze2[0].length];
+
+		// SOLVE MAZE 1
+		// --------------------------
+		System.out.println("SOLVING MAZE 1");
+		System.out.println("--------------------------");
+		// Traverse to find the start
+
+
+
+		// Call the solve method
+
+
+		System.out.println("--------------------------");
+
+
+
+
+		// SOLVE MAZE 2
+		// --------------------------
+		System.out.println("SOLVING MAZE 2");
+		System.out.println("--------------------------");
+		// Traverse to find the start
+
+
+
+		// Call the solve method
+
+
+		System.out.println("--------------------------");
+
+
 	}
 }
